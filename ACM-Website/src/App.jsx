57e6af6e29ffcf86689867a162443f246efbebc0
n2ai <longhai2511@gmail.com>
2023-04-18 20:49:23 -0500

    HaiTrinh

--- ACM-Website/src/App.jsx
+++ ACM-Website/src/App.jsx
@@ -4,15 +4,30 @@ import Banner from './components/Banner'
 import Navigator from './components/Navigator';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BackgroundVideo from './components/BackgroundVideo';
+import { createBrowserRouter, createRoutesFromElements, Route, Link,Outlet, RouterProvider } from 'react-router-dom';
+const Root = ()=>{
+  return (<>
+  <Banner/>
+  <Navigator />
+  <div>
+    <Outlet />
+  </div>
+  </>)
+}
+
 function App() {
-  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Root />}>
+        <Route index element={<BackgroundVideo/>} />
+      </Route>
+    )
+  )
 
   return (
-    <div className="App">
-      <Banner />
-      <Navigator />
-      <BackgroundVideo />
-    </div>
+    
+      <RouterProvider router={router} />
+    
   )
 }
 
