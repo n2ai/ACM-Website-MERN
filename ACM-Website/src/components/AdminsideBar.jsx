0791d44bba51483eda3e3c1a8b973175fa864f50
Clay Cardenas <ogclaystick@gmail.com>
2023-04-24 19:26:41 -0500

    adminPanelTemplate

---
+++ ACM-Website/src/components/AdminsideBar.jsx
@@ -0,0 +1,19 @@
+// VerticalButtons.js
+
+import React from 'react';
+
+function VerticalButtons() {
+  return (
+    <div className="vertical-buttons">
+      <button className="btn btn-warning">Button 1</button>
+      <button className="btn btn-warning">Button 2</button>
+      <button className="btn btn-warning">Button 3</button>
+      <button className="btn btn-warning">Button 4</button>
+      <button className="btn btn-warning">Button 5</button>
+      <button className="btn btn-warning">Button 6</button>
+      <button className="btn btn-warning">Button 7</button>
+    </div>
+  );
+}
+
+export default VerticalButtons;
