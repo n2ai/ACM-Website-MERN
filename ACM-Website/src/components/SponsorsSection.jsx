339d93a772909fdff468889dd541f7ca14bed848
n2ai <longhai2511@gmail.com>
2023-04-27 21:29:06 -0500

    StaffsSection Updated

---
+++ ACM-Website/src/components/SponsorsSection.jsx
@@ -0,0 +1,6 @@
+export default function SponsorsSection(){
+    return(
+        <>
+        </>
+    )
+}
--- ACM-Website/src/components/StaffsSection.jsx
+++ ACM-Website/src/components/StaffsSection.jsx
@@ -1,53 +1,37 @@
 import Carousel from 'react-bootstrap/Carousel';
 import StaffCard from './StaffCard';
 import {useEffect, useState} from 'react';
+import fakeImg from '../../images/portrait1.jpg'
 function StaffsSection() {
 
   return (
+    <>
     <section className='StaffsSection'>
+    <div className='StaffsSection-title'>
+        <h1>Meet Our Staffs</h1>
+      </div>
       <div className='StaffsSection-wrapper'>
         <div className='StaffsSection-card'>
           <div className='StaffsSection-card-img'> 
-            <img src="" alt='profile-img'></img>
+            <img src={fakeImg} alt='profile-img'></img>
           </div>
+          <div className='StaffsSection-card-socialMedias'>
+            <span>Linkedin</span>
+            <span>GitHub</span>
+          </div>
+          <br></br>
+          <br></br>
           <div className='StaffsSection-card-title'>
             <p>Title</p>
           </div>
           <div className='StaffsSection-card-name'></div>
             <p>Name</p>
         </div>
-        <div className='StaffsSection-card'>
-          <div className='StaffsSection-card-img'> 
-            <img src="" alt='profile-img'></img>
-          </div>
-          <div className='StaffsSection-card-title'>
-            <p>Title</p>
-          </div>
-          <div className='StaffsSection-card-name'></div>
-            <p>Name</p>
-        </div>
-        <div className='StaffsSection-card'>
-          <div className='StaffsSection-card-img'> 
-            <img src="" alt='profile-img'></img>
-          </div>
-          <div className='StaffsSection-card-title'>
-            <p>Title</p>
-          </div>
-          <div className='StaffsSection-card-name'></div>
-            <p>Name</p>
-        </div>
-        <div className='StaffsSection-card'>
-          <div className='StaffsSection-card-img'> 
-            <img src="" alt='profile-img'></img>
-          </div>
-          <div className='StaffsSection-card-title'>
-            <p>Title</p>
-          </div>
-          <div className='StaffsSection-card-name'></div>
-            <p>Name</p>
-        </div>
+        
       </div>
+      
     </section>
+    </>
   );
 }
 
