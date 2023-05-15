dba9dd01f81c19133fabc0d19742d52fe51a8e24
n2ai <trinhh7@gator.uhd.edu>
2023-05-15 16:55:05 +0700

    spnsors section update2

--- ACM-Website/src/components/SponsorsSection.jsx
+++ ACM-Website/src/components/SponsorsSection.jsx
@@ -7,9 +7,10 @@ export default function SponsorsSection({sponsorList}){
             <div className="sponsors-section">
                 <h1>Our Sponsors</h1>
                 <div className="sponsors-section-items-wrapper">
-                    <img className="sponsors-section-item" src="../../images/ACMlogo.png"></img>
-                    <img className="sponsors-section-item" src="../../images/ACMlogo.png"></img>
-                    <img className="sponsors-section-item" src="../../images/ACMlogo.png"></img>
+                    <a href="#"><img className="sponsors-section-item" src="../../images/ACMlogo.png"></img></a>
+                    <a href="#"><img className="sponsors-section-item" src="../../images/ACMlogo.png"></img></a>
+                    <a href="#"><img className="sponsors-section-item" src="../../images/ACMlogo.png"></img></a>
+
 
                 </div>
                 <div className="sponsors-section-contact">
